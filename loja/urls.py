ae858166bc4d8f9081d6730927311620e584d8e7
Lopes, Maycon <lopes.maycon@gmail.com>
2022-07-09 07:02:47 -0300

    Created product details template and view

--- loja/urls.py
+++ loja/urls.py
@@ -3,5 +3,6 @@ from loja import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detalhes', views.product_details, name='product_details'),
 
 ]
--- loja/views.py
+++ loja/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def product_details(request):
+    return render(request, 'product_details.html')
