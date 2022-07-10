1568ea109afa264709a1e24fba5ba446aa8ddef2
Lopes, Maycon <lopes.maycon@gmail.com>
2022-07-10 11:37:52 -0300

    Now, Product Model has credit_price attribute and in_stock boolean to determine whether product is in stock. The quantity attribute was removed.

--- loja/models.py
+++ loja/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
 
 class Product(models.Model):
@@ -35,9 +36,10 @@ class Product(models.Model):
     aquisition_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Preço de aquisição')
     aquisition_data = models.DateField(verbose_name='Data de aquisição')
     sell_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Preço de venda")
+    credit_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Preço a prazo")
     genre = models.CharField(max_length=6, choices=CATEGORY_CHOICES, verbose_name="Gênero")
-    size = models.CharField(max_length=2, choices=SIZE_CHOICES, verbose_name="Tamanho")
-    quantity = models.PositiveIntegerField(verbose_name='Quantidade em estoque')
+    size = MultiSelectField(choices=SIZE_CHOICES, verbose_name="Tamanho", blank=True)
+    in_stock = models.BooleanField(verbose_name='Em estoque', default=False, editable=False)
 
     class Meta:
         verbose_name = 'Produto'
@@ -45,3 +47,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.description[:255]
+
+    def save(self, *args, **kwargs):
+        """overrides save method to change in_stock attribute"""
+        if len(self.size):
+            self.in_stock = True
+        else:
+            self.in_stock = False
+
+        return super().save(*args, **kwargs)
