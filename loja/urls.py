59a2ac2cfb528bc0c028fcfaf50dc9c1904d081d
Lopes, Maycon <lopes.maycon@gmail.com>
2022-07-10 11:35:11 -0300

    Adjusted 'detalhes' url to match the ID fo eache product on database

--- loja/urls.py
+++ loja/urls.py
@@ -3,6 +3,6 @@ from loja import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('detalhes', views.product_details, name='product_details'),
+    path('detalhes/<int:pk>', views.product_details, name='product_details'),
 
 ]
