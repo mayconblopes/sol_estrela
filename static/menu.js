254c94327e6782512135c6a3f708f0427fc435db
Lopes, Maycon <lopes.maycon@gmail.com>
2022-07-13 18:51:06 -0300

    Now lateral menu closes when clickig outiside of it

--- static/menu.js
+++ static/menu.js
@@ -4,3 +4,10 @@ const menu = document.querySelector('.menu-lateral');
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo');
 })
+
+window.addEventListener('mouseup', function(event){
+    const menu = document.querySelector('.menu-lateral--ativo');
+    if (event.target != menu && event.target.parentNode != menu){
+        menu.classList.toggle('menu-lateral--ativo');
+    }
+});
