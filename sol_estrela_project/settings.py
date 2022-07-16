46f0f7cc375d32fb3242d0f93636db5310bfe03c
Lopes, Maycon <lopes.maycon@gmail.com>
2022-07-16 10:49:20 -0300

    Preparing for deployment

--- sol_estrela_project/settings.py
+++ sol_estrela_project/settings.py
@@ -151,8 +151,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = ['static']
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 django_on_heroku.settings(locals())
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    #'django.contrib.staticfiles.finders.AppDirectoriesFinder',    #causes verbose duplicate notifications in django 1.9
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
