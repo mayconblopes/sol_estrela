694a8811a99d69fcf708c015c2ee5a18dcc95afd
Lopes, Maycon <lopes.maycon@gmail.com>
2022-07-16 13:54:02 -0300

    AWS Configurations for media files

--- sol_estrela_project/settings.py
+++ sol_estrela_project/settings.py
@@ -166,3 +166,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AWS CONFIGURATIONS
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID') if os.getenv('AWS_ACCESS_KEY_ID') else config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY') if os.getenv('AWS_SECRET_ACCESS_KEY') else config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME') if os.getgetenv('AWS_STORAGE_BUCKET_NAME') else config('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = True
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
